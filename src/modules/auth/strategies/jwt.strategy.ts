f9769d5c8096ad19818fc5e4cf6566cbcc6c6f5d
em <mhdnris@gmail.com>
2024-10-03 16:22:43 +0330

    _

---
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { JwtService } from '@nestjs/jwt';
+import globallConfig from 'src/configs/global.config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly jwtService: JwtService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: globallConfig.jwt_secret,
+    });
+  }
+
+  async validate(payload: any) {
+    return { userId: payload.userId };
+  }
+}
