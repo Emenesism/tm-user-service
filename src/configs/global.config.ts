641d837bc8ff2f18d020376547a6b2694bdd9d1b
em <mhdnris@gmail.com>
2024-10-03 16:21:38 +0330

    add the jwt-secret to the config

--- src/configs/global.config.ts
+++ src/configs/global.config.ts
@@ -3,7 +3,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const globallConfig: any = {
-  env: process.env.NODE_ENV,
+  jwt_secret: process.env.JWT_SECRET,
   db: {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
