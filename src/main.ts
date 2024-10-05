b8ee1f01d9a6d4e46cf15c0916303cc92ac0b26a
em <mhdnris@gmail.com>
2024-10-05 16:01:05 +0330

    add validation pipe and read the port from the config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import globallConfig from './configs/global.config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(globallConfig.server.port);
 }
 bootstrap();
