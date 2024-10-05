378067dc34281812e4d740e646f4d040cf0e2bfa
em <mhdnris@gmail.com>
2024-10-05 16:01:27 +0330

    router for signup and login and the auth module

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { SignUpDTO } from './dto/sign-up.dto';
+import { UserRepositoryResponse } from '../user/interfaces/user-repo-res.interface';
+import { UsersService } from '../user/user.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly userSerice: UsersService) {}
+  @Post('signup')
+  async signUp(@Body() signUpData: SignUpDTO): Promise<UserRepositoryResponse> {
+    return this.userSerice.signup(signUpData);
+  }
+
+  @Post('login')
+  async login(
+    @Body() LoginData: Partial<SignUpDTO>,
+  ): Promise<UserRepositoryResponse> {
+    return this.userSerice.login(LoginData);
+  }
+}
