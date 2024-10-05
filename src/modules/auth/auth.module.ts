019f17971b365ba073e2b33fea8a61871a1c2141
em <mhdnris@gmail.com>
2024-10-05 16:02:53 +0330

    config the auth module and import provider and the user module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import globallConfig from 'src/configs/global.config';
+import { BcryptUtils } from 'src/common/utils/password.utils';
+import { UserModule } from '../user/user.module';
+import { UsersService } from '../user/user.service';
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: globallConfig.jwt_secret,
+      signOptions: { expiresIn: '48h' },
+    }),
+    UserModule,
+  ],
+  providers: [AuthService, BcryptUtils, UsersService],
   controllers: [AuthController],
-  providers: [AuthService]
+  exports: [JwtModule],
 })
 export class AuthModule {}
